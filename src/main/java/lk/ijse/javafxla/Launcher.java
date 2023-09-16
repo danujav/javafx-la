51ccdc4d08078395af7ec9568135c08d86f0a4f1
danu <danujagreru@gmail.com>
2023-09-16 15:42:22 +0530

    intial commit

---
+++ src/main/java/lk/ijse/javafxla/Launcher.java
@@ -0,0 +1,12 @@
+package lk.ijse.javafxla;
+
+/*
+    @author DanujaV
+    @created 9/16/23 - 3:37 PM   
+*/
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Main");
+    }
+}
