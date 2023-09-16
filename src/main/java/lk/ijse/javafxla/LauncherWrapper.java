313b50001de4315692f7f50539655f5c37aa2ca4
danu <danujagreru@gmail.com>
2023-09-16 15:49:42 +0530

    create wrapper class

---
+++ src/main/java/lk/ijse/javafxla/LauncherWrapper.java
@@ -0,0 +1,12 @@
+package lk.ijse.javafxla;
+
+/*
+    @author DanujaV
+    @created 9/16/23 - 3:48 PM   
+*/
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
